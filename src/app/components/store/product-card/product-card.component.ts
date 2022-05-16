5e59b06db954c70f30e09aa03611f98c8ebdb309
salvego <fael_ssantos@yahoo.com.br>
2022-05-15 23:23:31 -0300

    Initial commit

---
+++ src/app/components/store/product-card/product-card.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartUtil } from 'src/app/utils/cart.util';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html'
+})
+export class ProductCardComponent implements OnInit {
+  @Input() product!: Product;
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+  }
+  
+  //adicionar itens no carrinho
+  addToCart() {
+    CartUtil.add(
+      this.product._id,
+      this.product.title,
+      1,
+      this.product.price,
+      this.product.images[0]
+    )
+    this.toastr.success(`${this.product.title} adicionado ao carrinho`, 'Produto Adicionado');
+  }
+
+
+
+}
